b71ec58fd9d3bc266888304a03afb8448596c336
haih <haivictorh@mail.tau.ac.il>
2022-10-07 15:53:22 +0300

    Update softclip.py

--- experiments/measurements_distributions/linear_truncated_gaussian/softclip.py
+++ experiments/measurements_distributions/linear_truncated_gaussian/softclip.py
@@ -1,10 +1,24 @@
 import torch
 import numpy as np
 
-SLOP = 3.3
+SLOP = 1.0
 
 
-def soft_clip(x, a, b):
-    c = (b - a)
-    z = torch.arctan(SLOP * (x - a) / c) / np.pi + 0.5  # [0,1]
-    return c * z + a
+def soft_clip(x, a, b, r):
+    z = x.clone()
+    # c = (b - a)
+    z[x > b - r] = b - r + r * (1 - torch.exp(-torch.abs(x[x > b - r] - b + r)))
+    z[x < a + r] = a + r - r * (1 - torch.exp(-torch.abs(x[x < a + r] - a - r)))
+
+    return z
+
+
+if __name__ == '__main__':
+    x = torch.linspace(-10, 10, 10000)
+    y = soft_clip(x, -0, 5, 0.5)
+    from matplotlib import pyplot as plt
+
+    plt.plot(x.numpy(), y.numpy())
+    plt.plot(x.numpy(), x.numpy())
+    plt.grid()
+    plt.show()
